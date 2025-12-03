f304b3d274fcf8a671d6d75faf4e494033aa486c
okietokie <kalyanisanthoshkumar@gmail.com>
2025-12-03 08:52:33 +0400

    projects basic

--- peerCheck-frontend/src/api/axiosClient.js
+++ peerCheck-frontend/src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  //baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`, //"http://localhost:5000/api",
-  baseURL: `http://localhost:5000/api`
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`, //"http://localhost:5000/api",
+  //baseURL: `http://localhost:5000/api`
 });
 
 axiosClient.interceptors.request.use((config) => {
